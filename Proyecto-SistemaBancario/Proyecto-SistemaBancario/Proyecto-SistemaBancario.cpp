657f2e0b66c3f79614c49ee606c9e80bce8189a5
Gabriel <gevr0317990317@gmail.com>
2023-09-28 16:43:47 -0600

    Revert "Coloque blablabla"
    This reverts commit bf158b3eab735eb06447399caf1cac9f587da704.

--- Proyecto-SistemaBancario/Proyecto-SistemaBancario/Proyecto-SistemaBancario.cpp
+++ Proyecto-SistemaBancario/Proyecto-SistemaBancario/Proyecto-SistemaBancario.cpp
@@ -1,8 +1,12 @@
+// Proyecto-SistemaBancario.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Prueba";
+    std::cout << "Hello World!\n";
+
+    //holholaprueba
 }
